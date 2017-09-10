eb1b3c01e7ae7df90f1ac8468d70654dad039727
Tatyana Milanova <tanyta78@gmail.com>
2017-09-10 18:35:50 +0300

    organize

---
+++ LDS_Stacks_Queues/LDS_Stacks_Queues/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace LDS_Stacks_Queues
+{
+    using System;
+  
+    public class StartUp
+    {
+       public static void Main()
+       {
+          
+           
+       }
+    }
+}
--- LinearDataStructures/01. Data-Structures-Lab-Skeleton/Linear-Data-Structures/Lists/ArrayList.cs
+++ LinearDataStructures/01. Data-Structures-Lab-Skeleton/Linear-Data-Structures/Lists/ArrayList.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//
+//Implement a data structure ArrayList<T> that holds a sequence of elements of generic type T.It should hold a sequence of items in an array.The structure should have capacity that grows twice when it is filled, always starting at 2. The list should support the following operations:
+//•	int Count  returns the number of elements in the structure 
+//•	T this[int index]  the indexer should access the elements by index (in range 0 … Count-1) in the reverse order of adding
+//•	void Add(T item)  adds an element to the sequence(grow twice the underlying array to extend its capacity in case the capacity is full)
+//•	T RemoveAt(int index)  removes an element by index(in range 0 … Count-1) and returns the element
+//Be sure to test implemented operations whenever possible before moving to the next
+
+using System;
 
 public class ArrayList<T>
 {
