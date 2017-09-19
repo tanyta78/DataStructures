01dcd39ac93e5581b22915d2e3f79c48078504bf
Tatyana Milanova <tanyta78@gmail.com>
2017-09-19 07:13:43 +0300

    Heap lab

--- 05. Data-Structures-Heaps-Priority-Queues-Skeleton/BinaryHeap/BinaryHeap.cs
+++ 05. Data-Structures-Heaps-Priority-Queues-Skeleton/BinaryHeap/BinaryHeap.cs
@@ -7,29 +7,91 @@ public class BinaryHeap<T> where T : IComparable<T>
 
     public BinaryHeap()
     {
-        // TODO
+        this.heap = new List<T>();
     }
 
     public int Count
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get { return this.heap.Count; }
     }
 
     public void Insert(T item)
     {
-        throw new NotImplementedException();
+        this.heap.Add(item);
+
+        this.HeapifyUp(this.heap.Count - 1);
+    }
+
+    private void HeapifyUp(int index)
+    {
+        int parent = (index - 1) / 2;
+
+        while (this.IsGreater(index, parent))
+        {
+            this.Swap(index, parent);
+            index = parent;
+            parent = (index - 1) / 2;
+        }
+
+    }
+
+    private void Swap(int a, int b)
+    {
+        T temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+
+    private bool IsGreater(int index, int parent)
+    {
+        return this.heap[index].CompareTo(this.heap[parent]) > 0;
+
     }
 
     public T Peek()
     {
-        throw new NotImplementedException();
+        if (this.heap.Count <= 0)
+        {
+            throw new InvalidOperationException();
+        }
+        return this.heap[0];
     }
 
     public T Pull()
     {
-        throw new NotImplementedException();
+        if (this.heap.Count <=0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T result = this.heap[0];
+        this.Swap(0,this.Count-1);
+        this.heap.RemoveAt(this.Count - 1);
+        this.HeapifyDown(0);
+        
+
+        return result;
+    }
+
+    private void HeapifyDown(int index)
+    {
+        while (index < this.Count/2)
+        {
+            int child = 2 * index + 1;
+
+            if (child+1<this.Count && this.IsGreater(child+1,child))
+            {
+                child++;
+            }
+
+            if (this.IsGreater(index,child))
+            {
+                break;
+            }
+
+            this.Swap(child,index);
+
+            index = child;
+        }
     }
 }
