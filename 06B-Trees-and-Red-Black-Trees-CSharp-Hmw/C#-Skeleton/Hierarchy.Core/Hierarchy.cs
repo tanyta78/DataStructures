6d0671d09a4cc4f53d6f16418eec9bd6982f43e2
Tanya Milanova <tanyta78@gmail.com>
2019-01-22 16:23:52 +0200

    Exam DataStructures -Hierarchi - implementation

--- 06B-Trees-and-Red-Black-Trees-CSharp-Hmw/C#-Skeleton/Hierarchy.Core/Hierarchy.cs
+++ 06B-Trees-and-Red-Black-Trees-CSharp-Hmw/C#-Skeleton/Hierarchy.Core/Hierarchy.cs
@@ -1,62 +1,146 @@
 ﻿namespace Hierarchy.Core
 {
     using System;
-    using System.Collections.Generic;
     using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Hierarchy<T> : IHierarchy<T>
     {
+        private Node<T> root;
+        private Dictionary<T, Node<T>> nodes;
+
         public Hierarchy(T root)
         {
-            throw new NotImplementedException();
+            this.root = new Node<T>(root);
+            this.nodes = new Dictionary<T, Node<T>>
+            {
+                { root, this.root }
+            };
         }
 
-        public int Count
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int Count => this.nodes.Count;
 
         public void Add(T element, T child)
         {
-            throw new NotImplementedException();
+            if (element == null || child == null)
+            {
+                throw new ArgumentException();
+            }
+
+            if (!this.Contains(element) || this.Contains(child))
+            {
+                throw new ArgumentException();
+            }
+
+            var parentNode = this.nodes[element];
+            var childNode = new Node<T>(child)
+            {
+                Parent = parentNode
+            };
+
+            parentNode.Children.Add(childNode);
+            this.nodes.Add(child, childNode);
         }
 
         public void Remove(T element)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(element))
+            {
+                throw new ArgumentException();
+            }
+
+            var nodeElement = this.nodes[element];
+
+            if (nodeElement.Parent == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var parentNode = nodeElement.Parent;
+            var nodeChildren = nodeElement.Children;
+            foreach (var child in nodeChildren)
+            {
+                child.Parent = parentNode;
+                parentNode.Children.Add(child);
+
+            }
+
+            parentNode.Children.Remove(nodeElement);
+            this.nodes.Remove(element);
         }
 
         public IEnumerable<T> GetChildren(T item)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(item))
+            {
+                throw new ArgumentException();
+            }
+
+            var parent = this.nodes[item];
+
+            return parent.Children.Select(x => x.Value);
         }
 
         public T GetParent(T item)
         {
-            throw new NotImplementedException();
+            if (!this.Contains(item))
+            {
+                throw new ArgumentException();
+            }
+
+            var parent = this.nodes[item].Parent;
+
+            return parent != null ? parent.Value : default(T);
         }
 
         public bool Contains(T value)
         {
-            throw new NotImplementedException();
+            return this.nodes.ContainsKey(value);
         }
 
         public IEnumerable<T> GetCommonElements(Hierarchy<T> other)
         {
-            throw new NotImplementedException();
-        } 
+            var result = this.nodes.Keys.Intersect(other.nodes.Keys);
+            return result;
+        }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var queue = new Queue<Node<T>>();
+
+            var current = this.root;
+            queue.Enqueue(current);
+
+            while (queue.Count > 0)
+            {
+                current = queue.Dequeue();
+                yield return current.Value;
+                foreach (var child in current.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        private class Node<T>
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Children = new List<Node<T>>();
+                this.Parent = null;
+            }
+
+            public T Value { get; set; }
+            public List<Node<T>> Children { get; set; }
+            public Node<T> Parent { get; set; }
+
         }
     }
 }
